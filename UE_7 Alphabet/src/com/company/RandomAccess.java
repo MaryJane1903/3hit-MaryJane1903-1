2b6048b90bad86815f4438f31fe745d5cad4e293
Marin Antunovic <marin.antunovic@hotmail.com>
2017-02-28 17:23:32 +0100

    Stand 27.2.1017

---
+++ UE_7 Alphabet/src/com/company/RandomAccess.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.nio.file.*;
+import java.nio.ByteBuffer;
+/**
+ * Created by Marin on 27.02.2017.
+ */
+public class RandomAccess {
+
+    private Path file= Paths.get("C:\\Users\\Marin\\Desktop\\schule\\SEW\\SEW_uebungen\\3hit-MaryJane1903-1\\UE_7 Alphabet\\Alphabet.txt");
+    private byte[] data;
+    private ByteBuffer out;
+    private String s;
+
+
+
+}
+
