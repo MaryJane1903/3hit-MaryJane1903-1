a5b7f6b6f0ad689c39a30602e909b902b8a63d6c
Marin Antunovic <marin.antunovic@hotmail.com>
2017-05-05 12:33:59 +0200

    Stand 5.5.2017
    übung mit Flowpanes und V/HBoxen hier überschrieben

--- UE_9 JavaFXtest/src/com/company/Main.java
+++ UE_9 JavaFXtest/src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.*;
 
@@ -19,21 +26,55 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Stage mit Button");
-
-        Group root = new Group(new Button("Hello"));
-        Scene scene = new Scene(root, 300, 100);
+    /*
+        HBox test=new HBox(10);
+        Label text=new Label("Name");
+        TextField field=new TextField();
+        Button b1=new Button("OK");
+        Button b2=new Button("Cancle");
+        test.getChildren().addAll(text,field,b1,b2);
+        Scene scene = new Scene(test);
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.DECORATED);
-        primaryStage.initModality(Modality.WINDOW_MODAL);
-        primaryStage.initOwner(null);
+        test.setStyle("-fx-padding: 20;");
+        test.setAlignment(Pos.CENTER);
+        b2.setMaxHeight(1000);
+        test.setFillHeight(false);
+        test.setHgrow(field, Priority.ALWAYS);
+        FlowPane flo=new FlowPane();
+
+
+
         primaryStage.show();
-        Rectangle2D bounds= Screen.getPrimary().getVisualBounds();
-        double x=bounds.getMinX()+(bounds.getWidth()- primaryStage.getWidth())/2.0;
-        double y=bounds.getMinY()+(bounds.getHeight()-primaryStage.getHeight())/2.0;
 
-        primaryStage.sizeToScene();
-        primaryStage.setX(x);
-        primaryStage.setY(y);
 
+    */
+
+
+    /*
+        VBox test=new VBox(10);
+        Label text=new Label("Name");
+        TextField field=new TextField();
+        Button b1=new Button("OK");
+        Button b2=new Button("Cancle");
+        test.getChildren().addAll(text,field,b1,b2);
+        Scene scene = new Scene(test);
+        primaryStage.setScene(scene);
+        test.setStyle("-fx-padding: 20;");
+        test.setAlignment(Pos.CENTER);
+        b2.setMaxHeight(1000);
+        test.setFillWidth(false);
+        test.setVgrow(field, Priority.ALWAYS);
+        primaryStage.show();
+    */
+
+
+        FlowPane flo=new FlowPane();
+        for(int i=1;i<=10;i++) {
+            flo.getChildren().add(new Button("Button" + i));
+        }
+            Scene scene = new Scene(flo);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
     }
-}
+
